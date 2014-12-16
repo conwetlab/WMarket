0395bbdb4d65a32009aec9f0fae7a1a32e6debb9
Aitor Magán <amagan@conwet.com>
2014-12-16 15:02:26 +0100

    Test users validatior

---
+++ src/test/java/org/fiware/apps/marketplace/model/validators/UserValidatorTest.java
@@ -0,0 +1,150 @@
+package org.fiware.apps.marketplace.model.validators;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.fiware.apps.marketplace.exceptions.ValidationException;
+import org.fiware.apps.marketplace.model.User;
+import org.junit.Test;
+
+public class UserValidatorTest {
+	
+	private UserValidator userValidator = new UserValidator();
+	
+	private static final String MISSING_FILEDS_MSG = "name, email and/or password cannot be null";
+	private static final String INVALID_LENGTH_BASIC = "%s is not valid. (min length: %d, max length: %d)";
+	
+	private User generateValidUser() {
+		User user = new User();
+		user.setUserName("userName");
+		user.setPassword("1234");
+		user.setEmail("example@example.com");
+		user.setCompany("EXAMPLE");
+		user.setDisplayName("Example Name");
+		
+		return user;
+	}
+	
+	private void assertInvalidUser(User user, String expectedMsg) {
+		try {
+			userValidator.validateUser(user);
+			failBecauseExceptionWasNotThrown(ValidationException.class);
+		} catch (ValidationException ex) {
+			assertThat(ex).hasMessage(expectedMsg);
+		}
+	}
+	
+	@Test
+	public void testValidBasicUser() throws ValidationException {
+		User user = new User();
+		user.setUserName("userName");
+		user.setPassword("1234");
+		user.setEmail("example@example.com");
+		
+		assertThat(userValidator.validateUser(user)).isTrue();
+	}
+	
+	@Test
+	public void testValidComplexUser() throws ValidationException {
+		User user = generateValidUser();
+		assertThat(userValidator.validateUser(user)).isTrue();
+	}
+	
+	@Test
+	public void testMissingUserName() {
+		// Generate a user without username
+		User user = generateValidUser();
+		user.setUserName(null);
+		
+		assertInvalidUser(user, MISSING_FILEDS_MSG);
+	}
+	
+	@Test
+	public void testMissingPassword() {
+		// Generate a user without password
+		User user = generateValidUser();
+		user.setPassword(null);
+		
+		assertInvalidUser(user, MISSING_FILEDS_MSG);
+	}
+	
+	@Test
+	public void testMissingMail() {
+		// Generate a user without mail
+		User user = generateValidUser();
+		user.setEmail(null);
+		
+		assertInvalidUser(user, MISSING_FILEDS_MSG);
+	}
+	
+	@Test
+	public void testUserNameTooShort() {
+		User user = generateValidUser();
+		user.setUserName("a");
+		
+		assertInvalidUser(user, String.format(INVALID_LENGTH_BASIC, "userName", 5, 15));
+	}
+	
+	@Test
+	public void testUserNameTooLong() {
+		User user = generateValidUser();
+		user.setUserName("1234567890123456");
+		
+		assertInvalidUser(user, String.format(INVALID_LENGTH_BASIC, "userName", 5, 15));
+	}
+	
+	@Test
+	public void testDisplayNameTooShort() {
+		User user = generateValidUser();
+		user.setDisplayName("a");
+		
+		assertInvalidUser(user, String.format(INVALID_LENGTH_BASIC, "displayName", 5, 30));
+	}
+	
+	@Test
+	public void testDisplayNameTooLong() {
+		User user = generateValidUser();
+		user.setDisplayName("1234567890123456789012345678901");
+		
+		assertInvalidUser(user, String.format(INVALID_LENGTH_BASIC, "displayName", 5, 30));
+	}
+	
+	@Test
+	public void testInvalidMail1() {
+		User user = generateValidUser();
+		user.setEmail("test");
+		
+		assertInvalidUser(user, "email is not valid");
+	}
+	
+	@Test
+	public void testInvalidMail2() {
+		User user = generateValidUser();
+		user.setEmail("test@test");
+		
+		assertInvalidUser(user, "email is not valid");
+	}
+	
+	@Test
+	public void testInvalidMail3() {
+		User user = generateValidUser();
+		user.setEmail("@test.com");
+		
+		assertInvalidUser(user, "email is not valid");
+	}
+	
+	@Test
+	public void testCompanyTooShort() {
+		User user = generateValidUser();
+		user.setCompany("a");
+		
+		assertInvalidUser(user, String.format(INVALID_LENGTH_BASIC, "company", 3, 30));
+	}
+	
+	@Test
+	public void testCompanyTooLong() {
+		User user = generateValidUser();
+		user.setCompany("1234567890123456789012345678901");
+		
+		assertInvalidUser(user, String.format(INVALID_LENGTH_BASIC, "company", 3, 30));
+	}
+}
