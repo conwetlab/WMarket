1d957e45bb46b108ba00be36bd78f83c46539a22
Aitor Magán García <amagan@conwet.com>
2015-04-21 18:44:51 +0200

    First integration test

---
+++ src/test/java/org/fiware/apps/marketplace/it/UsersServiceIT.java
@@ -0,0 +1,102 @@
+package org.fiware.apps.marketplace.it;
+
+/*
+ * #%L
+ * FiwareMarketplace
+ * %%
+ * Copyright (C) 2015 CoNWeT Lab, Universidad Politécnica de Madrid
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of copyright holders nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software 
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.junit.Test;
+
+import com.sun.xml.txw2.annotation.XmlElement;
+
+public class UsersServiceIT {
+	
+	private static final String END_POINT = "http://localhost:8080/FiwareMarketplace";
+	
+	@Test(timeout=100000000)
+	public void testUserCreation() throws InterruptedException {
+		
+		SerializableUser user = new SerializableUser();
+		user.setDisplayName("FIWARE Example");
+		user.setEmail("example_mail@fiware.com");
+		user.setPassword("password1!a");
+		
+		Client client = ClientBuilder.newClient();
+		Response a = client.target(END_POINT + "/api/v2/user").request(MediaType.APPLICATION_JSON).post(Entity.entity(user, MediaType.APPLICATION_JSON));
+						
+		assertThat(a.getStatus()).isEqualTo(201);
+		assertThat(a.getHeaderString("Location")).isEqualTo(END_POINT + "/api/v2/user/fiware-example");
+		
+	}
+	
+	public static class SerializableUser {
+		
+		private String displayName;
+		private String email;
+		private String password;
+		
+		@XmlElement
+		public String getDisplayName() {
+			return displayName;
+		}
+		
+		public void setDisplayName(String displayName) {
+			this.displayName = displayName;
+		}
+		
+		@XmlElement
+		public String getEmail() {
+			return email;
+		}
+		
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		
+		@XmlElement
+		public String getPassword() {
+			return password;
+		}
+		
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		
+	}
+
+}
